5701bce64ca4d54c158bf016b7f55a8c765ad6a1
SubahaSalman <subaha.salman@gmail.com>
2025-06-26 17:42:21 +0200

    and-or operator

---
+++ and-or-operator.py
@@ -0,0 +1,22 @@
+a=10
+b=14
+c=0
+
+if a and b and c:
+    print("All of the numbers have the boolean value set to True")
+else:
+    print("Atleast one of the numbers has the boolean value set to False.")
+
+a=10
+b=-5
+c=0
+
+if a>0 or b>0:
+    print("Atleast one of the numbers is greater than 0.")
+else:
+    print("Both of the numbers are less than zero")
+
+if b>0 or c>0:
+    print("Atleast one of the numbers is greater than 0")
+else:
+    print("None of the numbers are greater than zero.")
