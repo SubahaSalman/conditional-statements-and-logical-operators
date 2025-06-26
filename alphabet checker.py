d58f91187c33addeea71fbe91e71a942dc226c94
SubahaSalman <subaha.salman@gmail.com>
2025-06-26 18:08:47 +0200

    alphabet checker

---
+++ alphabet checker.py
@@ -0,0 +1,6 @@
+letter= input("Enter the character: ")
+
+if letter == "a" or letter == "b" or letter == "c" or letter == "d" or letter == "e" or letter == "f" or letter == "g" or letter == "h" or letter == "i" or letter == "j" or letter == "k" or letter == "l" or letter == "m" or letter == "n" or letter == "o" or letter == "p" or letter == "q" or letter == "r" or letter == "s" or letter == "t" or letter == "u" or letter == "v" or letter == "w" or letter == "x" or letter == "y" or letter == "z":
+    print("This character is part of the alphabet!")
+else:
+    print("This character is not part of the alphabet!")
