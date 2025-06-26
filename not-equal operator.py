e1025d03e78e5bad29cd1537f348ee383d6fd49d
SubahaSalman <subaha.salman@gmail.com>
2025-06-26 17:48:47 +0200

    not equal operator

---
+++ not-equal operator.py
@@ -0,0 +1,18 @@
+a=10
+b=12
+c=12
+
+print(a != b)
+print(b != c)
+
+a= "python"
+b= "coding"
+
+if a != b:
+    print(a, "and", b, "are not the same.")
+
+a=4
+b=5
+
+if (a==1) != (b==4):
+    print("Hi")
